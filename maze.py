3981a900c7a73000116e8d2797878a1d5664069c
AlgoClassWork <kyxec.play@gmail.com>
2026-05-10 17:48:42 +0600

    Update maze.py

--- maze.py
+++ maze.py
@@ -1,17 +1,23 @@
 # pip install pygame
 from pygame import *
 
+class GameSprite():
+    def __init__(self, img, x, y):
+        self.image = transform.scale(image.load(img), (50, 50))
+        self.x = x 
+        self.y = y
+
+    def show(self):
+        window.blit( self.image, (self.x, self.y) )
+
 window = display.set_mode( (700, 500) )
 display.set_caption('Лабиринт')
 
 background = image.load('background.jpg')
 background = transform.scale(background, (700, 500))
 
-hero = image.load('hero.png')
-hero = transform.scale(hero, (50, 50))
-
-hero_x = 300
-hero_y = 300
+hero = GameSprite('hero.png', 200, 200)
+enemy = GameSprite('cyborg.png', 400, 200)
 
 clock = time.Clock()
 
@@ -21,17 +27,18 @@ while True:
             exit()
 
     window.blit( background, (0, 0) )
-    window.blit( hero, (hero_x, hero_y) )
+    hero.show()
+    enemy.show()
 
     keys = key.get_pressed()
-    if keys[K_w] and hero_y > 0:
-        hero_y -= 10
-    if keys[K_s] and hero_y < 450:
-        hero_y += 10
-    if keys[K_a] and hero_x > 0:
-        hero_x -= 10
-    if keys[K_d] and hero_x < 650:
-        hero_x += 10
+    if keys[K_w] and hero.y > 0:
+        hero.y -= 10
+    if keys[K_s] and hero.y < 450:
+        hero.y += 10
+    if keys[K_a] and hero.x > 0:
+        hero.x -= 10
+    if keys[K_d] and hero.x < 650:
+        hero.x += 10
 
     display.update()
     clock.tick(60)
